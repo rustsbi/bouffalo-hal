1acec866c898e9772249ffcc35e4c2a5fb1106e2
crk <U202012040@hust.edu.cn>
2023-05-09 15:21:20 +0800

    main: 使用clap分析命令行参数
    打印输入和输出文件的名称
    
    Signed-off-by: crk <U202012040@hust.edu.cn>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+
+/// Bouffalo ROM image helper
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Input ROM image filename
+    input: String,
+    /// Write output to <filename>
+    #[arg(short, long, value_name = "FILENAME")]
+    output: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("Input file name: {}!", args.input);
+    println!("Output file name: {:?}!", args.output);
 }
