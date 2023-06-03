f38a7e2469aa07928617483eedaf6bcc12903d12
zjx1319 <zjx1319@hust.edu.cn>
2023-06-03 16:13:16 +0800

    example: 添加gpio-demo测例
    gpio-demo测例来自https://gitee.com/rustsbi/bl-soc/pulls/2
    此外还优化了jtag-demo的命名，设置publish为false避免测例被发布
    
    Signed-off-by: zjx1319 <zjx1319@hust.edu.cn>

--- examples/jtag-demo/src/main.rs
+++ examples/jtag-demo/src/main.rs
@@ -1,6 +1,6 @@
 // Build this example with:
 // rustup target install riscv64imac-unknown-none-elf
-// cargo build --target riscv64imac-unknown-none-elf --release -p bl-soc-example-jtag-demo
+// cargo build --target riscv64imac-unknown-none-elf --release -p jtag-demo
 
 #![no_std]
 #![no_main]
