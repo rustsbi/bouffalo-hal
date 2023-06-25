916f15181b592fac94d4a4aabf303545366ffb7d
zjx1319 <zjx1319@hust.edu.cn>
2023-06-25 17:51:03 +0800

    bl616: 编译时生成镜像头
    Signed-off-by: zjx1319 <zjx1319@hust.edu.cn>

--- build.rs
+++ build.rs
@@ -8,6 +8,8 @@ fn main() {
     std::fs::write(ld, LINKER_SCRIPT).unwrap();
     #[cfg(feature = "bl808-d0")]
     std::fs::write(ld, LINKER_SCRIPT).unwrap();
+    #[cfg(feature = "bl616")]
+    std::fs::write(ld, LINKER_SCRIPT).unwrap();
 
     println!("cargo:rustc-link-arg=-T{}", ld.display());
     println!("cargo:rustc-link-search={}", out.display());
@@ -149,6 +151,76 @@ SECTIONS {
         *(.sbss .sbss.*)
         ebss = .;
     } > VRAM
+    /DISCARD/ : {
+        *(.eh_frame)
+    }
+}";
+
+#[cfg(feature = "bl616")]
+const LINKER_SCRIPT: &[u8] = b"
+OUTPUT_ARCH(riscv)
+ENTRY(_start)
+MEMORY {
+    PSEUDO_HEADER : ORIGIN = 0xA0000000 - 0x1000, LENGTH = 4K
+    FLASH : ORIGIN = 0xA0000000, LENGTH = 4M - 4K
+    OCRAM : ORIGIN = 0x62FC0000, LENGTH = 320K
+}
+SECTIONS {
+    .head : ALIGN(4) {
+        LONG(0x504E4642);
+        LONG(1);
+        KEEP(*(.head.flash));
+        KEEP(*(.head.clock));
+        KEEP(*(.head.base.flag));
+        LONG(ADDR(.text) - ORIGIN(PSEUDO_HEADER));
+        KEEP(*(.head.base.aes-region));
+        LONG(SIZEOF(.text) + SIZEOF(.rodata) + SIZEOF(.data));
+        KEEP(*(.head.base.hash));
+        KEEP(*(.head.cpu));
+        LONG(0);
+        LONG(0);
+        LONG(0);
+        LONG(0);
+        KEEP(*(.head.patch.on-read));
+        KEEP(*(.head.patch.on-jump));
+        LONG(0);
+        LONG(0);
+        LONG(0);
+        LONG(0);
+        LONG(0);
+        KEEP(*(.head.crc32));
+        FILL(0xFFFFFFFF);
+        . = ORIGIN(PSEUDO_HEADER) + LENGTH(PSEUDO_HEADER);
+    } > PSEUDO_HEADER
+    .text : ALIGN(4) {
+        stext = .;
+        KEEP(*(.text.entry))
+        *(.text .text.*)
+        . = ALIGN(4);
+        etext = .;
+    } > FLASH
+    .rodata : ALIGN(4) {
+        srodata = .;
+        *(.rodata .rodata.*)
+        *(.srodata .srodata.*)
+        . = ALIGN(4);
+        erodata = .;
+    } > FLASH
+    .data : ALIGN(4) {
+        sdata = .;
+        *(.data .data.*)
+        *(.sdata .sdata.*)
+        . = ALIGN(4);
+        edata = .;
+    } > OCRAM AT>FLASH
+    sidata = LOADADDR(.data);
+    .bss (NOLOAD) : ALIGN(4) {
+        *(.bss.uninit)
+        sbss = .;
+        *(.bss .bss.*)
+        *(.sbss .sbss.*)
+        ebss = .;
+    } > OCRAM
     /DISCARD/ : {
         *(.eh_frame)
     }
