d18658c2b30ee21819ef41a0a1a90d160b708379
Zhouqi Jiang <luojia@hust.edu.cn>
2023-07-16 21:39:27 +0800

    gpio: 添加结构体的详细文档
    Signed-off-by: Zhouqi Jiang <luojia@hust.edu.cn>

--- src/gpio.rs
+++ src/gpio.rs
@@ -10,6 +10,106 @@ use core::marker::PhantomData;
 use embedded_hal::digital::{ErrorType, InputPin, OutputPin};
 
 /// Individual GPIO pin.
+///
+/// Generic Purpose Input/Output, or GPIO, is a standard interface used to connect the
+/// microcontroller to external hardware. There are multiple GPIO pins on one chip,
+/// which are different due to the signal of the internal connections. This structure
+/// distinguishes them using constant generic number `N`.
+///
+/// This structure represents a code ownership of Generic Purpose Input/Output pin.
+/// GPIO pin structures are mutually exclusive, meaning that code in different contexts
+/// are allowed use the same GPIO pin at the same time. Ownerships pass from structure
+/// members to their wrappers; if one peripheral owns this pin, another peripheral
+/// cannot use it at the same time.
+///
+/// GPIO pin structure has an alternate mode parameter `M`. Alternate modes can be
+/// switched by using functions begin with `into_`. Those functions internally operate
+/// on hardware (specifically, GLB peripheral) to change pin function, and convert this
+/// structure to `Pin` type with different alternate mode generic parameters. With new
+/// alternate mode types, `Pin` structure would now match the demand of creating new
+/// peripheral structures, or include specific functions for developers to use.
+///
+/// # Examples
+///
+/// A simple usage of GPIO pin is digital signal output.
+///
+/// ```no_run
+/// # use base_address::Static;
+/// # use bl_soc::gpio::Pins;
+/// # pub struct Peripherals { gpio: Pins<Static<0x20000000>> }
+/// # fn main() -> ! {
+/// #   let p: Peripherals = unsafe { core::mem::transmute(()) };
+/// use embedded_hal::digital::{OutputPin, PinState};
+///
+/// // Switch io8 pin into floating output mode to prepare setting its state.
+/// // It's connected to an LED to be controlled on hardware.
+/// let mut led = p.gpio.io8.into_floating_output();
+/// // Define a software LED state to be written into hardware.
+/// let mut led_state = PinState::High;
+/// loop {
+///     // We use function `set_state` to write GPIO electronic level.
+///     // This function is provided by floating output pin through embedded-hal
+///     // crate; we can also use `set_high` or `set_low` here.
+///     led.set_state(led_state).ok();
+///     // Now we flip software LED state to make the LED blink in next loop.
+///     led_state = !led_state;
+///     // Delay for some time for human eyes to discover the blink.
+///     for _ in 0..100_000 {
+///         unsafe { core::arch::asm!("nop") }
+///     }
+/// }
+/// # }
+/// ```
+///
+/// Peripheral structures usually limits the alternate mode of a GPIO pin. It must be
+/// adjusted to the corresponding type before it can be used to create such structures.
+///
+/// ```no_run
+/// # use base_address::Static;
+/// # use embedded_time::rate::*;
+/// # use bl_soc::{
+/// #     clocks::Clocks,
+/// #     gpio::Pins,
+/// #     uart::{BitOrder, Config, Parity, Serial, StopBits, UartMuxes, WordLength},
+/// #     GLB, UART,
+/// # };
+/// # use embedded_io::blocking::Write;
+/// # pub struct Peripherals {
+/// #     gpio: Pins<Static<0x20000000>>,
+/// #     glb: GLB<Static<0x20000000>>,
+/// #     uart0: UART<Static<0x2000A000>>,
+/// #     uart_muxes: UartMuxes<Static<0x20000000>>,
+/// # }
+/// # fn main() {
+/// # let p: Peripherals = unsafe { core::mem::transmute(()) };
+/// # let clocks = Clocks {};
+/// // Prepare UART transmit and receive pins by converting io14 and io15 into
+/// // UART signal alternate mode.
+/// let tx = p.gpio.io14.into_uart();
+/// let rx = p.gpio.io15.into_uart();
+/// # let sig2 = p.uart_muxes.sig2.into_transmit::<0>();
+/// # let sig3 = p.uart_muxes.sig3.into_receive::<0>();
+/// # let config = Config {
+/// #     bit_order: BitOrder::LsbFirst,
+/// #     parity: Parity::None,
+/// #     stop_bits: StopBits::One,
+/// #     word_length: WordLength::Eight,
+/// # };
+/// // Create the serial structure. Note that if we don't have tx and rx GPIO
+/// // alternate mode set correctly, code here won't compile for type mismatch.
+/// let mut serial = Serial::new(
+///     p.uart0,
+///     config,
+///     2000000.Bd(),
+///     ((tx, sig2), (rx, sig3)),
+///     &clocks,
+///     &p.glb,
+/// );
+/// // Now that we have a working serial structure, we write something with it.
+/// writeln!(serial, "Hello world!").ok();
+/// serial.flush().ok();
+/// # }
+/// ```
 pub struct Pin<A: BaseAddress, const N: usize, M: Alternate> {
     #[cfg(any(feature = "glb-v1", feature = "glb-v2"))]
     pub(crate) base: GLB<A>,
