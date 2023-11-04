9fe1f4df2fd127d67179a466b9eb0ef26036df19
RenXiao <u202112004@hust.edu.cn>
2023-11-04 22:18:27 +0800

    usb:  完成usb/v1.rs及其单元测试 emac: 完成emac.rs及其单元测试
    Signed-off-by: RenXiao <u202112004@hust.edu.cn>

--- src/emac.rs
+++ src/emac.rs
@@ -1,7 +1,147 @@
 //! Ethernet Media Access Control peripheral.
+use volatile_register::{RO, RW};
 
 /// Ethernet Media Access Control peripheral registers.
 #[repr(C)]
 pub struct RegisterBlock {
-    // todo fields
+    /// Configuration
+    pub mode: RW<Mode>,
+    /// Transmit control
+    pub interrupt_source: RW<InterruptSource>,
+    /// Interrupt mask
+    pub interrupt_mask: RW<InterruptMask>,
+    /// Inter-packet gap (backed-gap only, non-backed-gap not seen in bl-docs)
+    pub backed_gap: RW<BackedGap>,
+    _reserved0: [u8; 0x08],
+    /// Frame length, seen in bl-docs as PACKETLEN
+    pub frame_length: RW<FrameLength>,
+    /// Collision config
+    pub collision: RW<Collision>,
+    /// transmit_buffer, seen in bl-docs as TX_BD_NUM
+    pub transmit_buffer: RW<TransmitBuffer>,
+    // Flow control, not seen in bl-docs, temporarily reserved
+    _reserved1: [u8; 0x04],
+    /// MII mode
+    pub mii_mode: RW<MiiMode>,
+    /// MII command
+    pub mii_command: RW<MiiCommand>,
+    /// MII address
+    pub mii_address: RW<MiiAddress>,
+    /// MII write control
+    pub control_write: RW<ControlWrite>,
+    /// MII read control
+    pub control_read: RW<ControlRead>,
+    /// MII state
+    pub mii_state: RO<MiiState>,
+    /// Media Access Control address 0 and 1
+    pub mac_address: [RW<MacAddress>; 2],
+    /// Hash 0 and 1
+    pub hash: [RW<Hash>; 2],
+    /// Transmit control
+    pub transmit_control: RW<TransmitControl>,
+}
+
+/// EMAC mode configuration register
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Default)]
+#[repr(transparent)]
+pub struct Mode(u32);
+
+/// EMAC transmit control register
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Default)]
+#[repr(transparent)]
+pub struct InterruptSource(u32);
+
+/// EMAC interrupt mask register
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Default)]
+#[repr(transparent)]
+pub struct InterruptMask(u32);
+
+/// EMAC inter packet gap (backed gap) register
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Default)]
+#[repr(transparent)]
+pub struct BackedGap(u32);
+
+/// EMAC frame length buffer
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Default)]
+#[repr(transparent)]
+pub struct FrameLength(u32);
+
+/// EMAC collision register
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Default)]
+#[repr(transparent)]
+pub struct Collision(u32);
+
+/// EMAC transmit buffer register
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Default)]
+#[repr(transparent)]
+pub struct TransmitBuffer(u32);
+
+/// MII clock divider and premable register
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Default)]
+#[repr(transparent)]
+pub struct MiiMode(u32);
+
+/// MII control data, read and scan state register
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Default)]
+#[repr(transparent)]
+pub struct MiiCommand(u32);
+
+/// MII physical layer bus address register
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Default)]
+#[repr(transparent)]
+pub struct MiiAddress(u32);
+
+/// MII write control register
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Default)]
+#[repr(transparent)]
+pub struct ControlWrite(u32);
+
+/// MII read control register
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Default)]
+#[repr(transparent)]
+pub struct ControlRead(u32);
+
+/// MII state register
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Default)]
+#[repr(transparent)]
+pub struct MiiState(u32);
+
+/// Media Access Control address register
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Default)]
+#[repr(transparent)]
+pub struct MacAddress(u32);
+
+/// hash register (64-bit to double 32-bit)
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Default)]
+#[repr(transparent)]
+pub struct Hash(u32);
+/// Transit control register
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Default)]
+#[repr(transparent)]
+pub struct TransmitControl(u32);
+
+#[cfg(test)]
+mod tests {
+    use super::RegisterBlock;
+    use memoffset::offset_of;
+
+    #[test]
+    fn struct_register_block_offset() {
+        assert_eq!(offset_of!(RegisterBlock, mode), 0x00);
+        assert_eq!(offset_of!(RegisterBlock, interrupt_source), 0x04);
+        assert_eq!(offset_of!(RegisterBlock, interrupt_mask), 0x08);
+        assert_eq!(offset_of!(RegisterBlock, backed_gap), 0x0c);
+        assert_eq!(offset_of!(RegisterBlock, frame_length), 0x18);
+        assert_eq!(offset_of!(RegisterBlock, collision), 0x1c);
+        assert_eq!(offset_of!(RegisterBlock, transmit_buffer), 0x20);
+        assert_eq!(offset_of!(RegisterBlock, mii_mode), 0x28);
+        assert_eq!(offset_of!(RegisterBlock, mii_command), 0x2c);
+        assert_eq!(offset_of!(RegisterBlock, mii_address), 0x30);
+        assert_eq!(offset_of!(RegisterBlock, control_write), 0x34);
+        assert_eq!(offset_of!(RegisterBlock, control_read), 0x38);
+        assert_eq!(offset_of!(RegisterBlock, mii_state), 0x3c);
+        assert_eq!(offset_of!(RegisterBlock, mac_address), 0x40);
+        assert_eq!(offset_of!(RegisterBlock, hash), 0x48);
+        assert_eq!(offset_of!(RegisterBlock, transmit_control), 0x50);
+    }
 }
--- src/usb/v1.rs
+++ src/usb/v1.rs
@@ -1,5 +1,7 @@
 //! Universal Serial Bus on BL702 series.
-use volatile_register::RW;
+use core::ops;
+
+use volatile_register::{RO, RW, WO};
 
 /// Universal Serial Bus register
 #[repr(C)]
@@ -10,15 +12,27 @@ pub struct RegisterBlock {
     pub usb_lpm_config: RW<UsbLpmConfig>,
     /// USB resume configuration
     pub usb_resume_config: RW<UsbResumeConfig>,
-    // /// USB frame number
-    //
-    // USB error
-    //
-    // USB interrupt enable
-    //
-    // USB interrupt status
-    //
-    // USB interrupt mask
+    _reserved0: [u8; 0x0c],
+    /// USB frame number
+    pub usb_frame_number: RO<UsbFrameNumber>,
+    /// USB error
+    pub usb_error: RO<UsbError>,
+    /// USB interrupt enable
+    pub usb_interrupt_enable: RW<UsbInterruptEnable>,
+    /// USB interrupt status
+    pub usb_interrupt_status: RO<UsbInterruptStatus>,
+    /// USB interrupt mask
+    pub usb_interrupt_mask: RW<UsbInterruptMask>,
+    /// USB interrupt clear
+    pub usb_interrupt_clear: WO<UsbInterruptClear>,
+    _reserved1: [u8; 0x10],
+    /// Endpoint configurations.
+    pub endpoint_config: ArrayProxy<RW<EndpointConfig>, 1, 7>,
+    _reserved2: [u8; 0xa4],
+    /// Endpoint FIFO registers.
+    pub endpoint_fifo: [EndpointFifo; 8],
+    /// Transceiver interface configuration.
+    pub transceiver_config: RW<TransceiverConfig>,
 }
 
 /// USB configuration register.
@@ -36,4 +50,110 @@ pub struct UsbLpmConfig(u32);
 #[repr(transparent)]
 pub struct UsbResumeConfig(u32);
 
-// /// USB frame number register.
+/// USB frame number register.
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Default)]
+#[repr(transparent)]
+pub struct UsbFrameNumber(u32);
+
+/// USB error register.
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Default)]
+#[repr(transparent)]
+pub struct UsbError(u32);
+
+/// USB interrupt enable register.
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Default)]
+#[repr(transparent)]
+pub struct UsbInterruptEnable(u32);
+
+/// USB interrupt status register.
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Default)]
+#[repr(transparent)]
+pub struct UsbInterruptStatus(u32);
+
+/// USB interrupt mask register.
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Default)]
+#[repr(transparent)]
+pub struct UsbInterruptMask(u32);
+
+/// USB interrupt clear register.
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Default)]
+#[repr(transparent)]
+pub struct UsbInterruptClear(u32);
+
+/// Endpoint configuration register.
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Default)]
+#[repr(transparent)]
+pub struct EndpointConfig(u32);
+
+/// Endpoint FIFO configurations.
+#[repr(C)]
+pub struct EndpointFifo {
+    /// Endpoint FIFO configuration register.
+    pub fifo_config: RW<FifoConfig>,
+    /// Endpoint FIFO state register.
+    pub fifo_status: RW<FifoStatus>,
+    /// Write data into first-in first-out queue.
+    pub data_write: WO<u32>,
+    /// Read data from first-in first-out queue.
+    pub data_read: RO<u32>,
+}
+
+/// Endpoint FIFO configuration register.
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Default)]
+#[repr(transparent)]
+pub struct FifoConfig(u32);
+
+/// Endpoint FIFO state register.
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Default)]
+#[repr(transparent)]
+pub struct FifoStatus(u32);
+
+/// Transceiver interface configuration.
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Default)]
+#[repr(transparent)]
+pub struct TransceiverConfig(u32);
+
+/// Array index helper structure.
+#[repr(C)]
+pub struct ArrayProxy<T, const S: usize, const N: usize> {
+    inner: [T; N],
+}
+
+impl<T, const S: usize, const N: usize> ops::Index<usize> for ArrayProxy<T, S, N> {
+    type Output = T;
+
+    #[inline]
+    fn index(&self, index: usize) -> &Self::Output {
+        assert!(index >= S && index < S + N);
+        &self.inner[index - S]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{EndpointFifo, RegisterBlock};
+    use memoffset::offset_of;
+
+    #[test]
+    fn struct_register_block_offset() {
+        assert_eq!(offset_of!(RegisterBlock, usb_config), 0x00);
+        assert_eq!(offset_of!(RegisterBlock, usb_lpm_config), 0x04);
+        assert_eq!(offset_of!(RegisterBlock, usb_resume_config), 0x08);
+        assert_eq!(offset_of!(RegisterBlock, usb_frame_number), 0x18);
+        assert_eq!(offset_of!(RegisterBlock, usb_error), 0x1c);
+        assert_eq!(offset_of!(RegisterBlock, usb_interrupt_enable), 0x20);
+        assert_eq!(offset_of!(RegisterBlock, usb_interrupt_status), 0x24);
+        assert_eq!(offset_of!(RegisterBlock, usb_interrupt_mask), 0x28);
+        assert_eq!(offset_of!(RegisterBlock, usb_interrupt_clear), 0x2c);
+        assert_eq!(offset_of!(RegisterBlock, endpoint_config), 0x40);
+        assert_eq!(offset_of!(RegisterBlock, endpoint_fifo), 0x100);
+    }
+
+    #[test]
+    fn struct_endpoint_fifo_offset() {
+        assert_eq!(offset_of!(EndpointFifo, fifo_config), 0x00);
+        assert_eq!(offset_of!(EndpointFifo, fifo_status), 0x04);
+        assert_eq!(offset_of!(EndpointFifo, data_write), 0x08);
+        assert_eq!(offset_of!(EndpointFifo, data_read), 0x0c);
+    }
+}
