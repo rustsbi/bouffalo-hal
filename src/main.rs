fcecba621e7c8ffd87128794db44c5d0e286d230
DongQing <placebo27@hust.edu.cn>
2024-11-02 14:26:03 +0800

    main: add embedded-cli based serial command line console support
    - Add embedded-cli based serial command line console support for bouffaloader,
    which features the command-line menu with autocomplete, history, and help command.
    - Add flash guide for bouffaloader on Windows in `README.md`, with which
    we can have a test on M1s Dock.
    
    Refs: https://github.com/rustsbi/bouffalo-hal/pull/4
    Refs: https://github.com/rustsbi/bouffalo-hal/pull/5
    Signed-off-by: DongQing <placebo27@hust.edu.cn>

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,30 @@
 
 use bouffalo_hal::prelude::*;
 use bouffalo_rt::{entry, Clocks, Peripherals};
+use embedded_cli::{cli::CliBuilder, Command};
 use embedded_time::rate::*;
 use panic_halt as _;
+
+#[derive(Command)]
+enum Base {
+    /// Print out 'Hello world!'.
+    Hello,
+    /// LED control command.
+    Led {
+        #[command(subcommand)]
+        command: Option<LedCommand>,
+    },
+}
+
+#[derive(Command)]
+enum LedCommand {
+    /// Turn on LED.
+    On,
+    /// Turn off LED.
+    Off,
+    /// Switch LED state.
+    Switch,
+}
 
 #[entry]
 fn main(p: Peripherals, c: Clocks) -> ! {
@@ -14,51 +36,50 @@ fn main(p: Peripherals, c: Clocks) -> ! {
     let sig3 = p.uart_muxes.sig3.into_receive::<0>();
 
     let config = Default::default();
-    let mut serial = p
+    let serial = p
         .uart0
         .freerun(config, 2000000.Bd(), ((tx, sig2), (rx, sig3)), &c);
 
+    let (mut tx, mut rx) = serial.split();
+
     let mut led = p.gpio.io8.into_floating_output();
     let mut led_state = PinState::Low;
-    let mut buf = [0u8; 32];
-    let mut ch = b'\r';
 
-    writeln!(serial, "Welcome to console example by bouffalo-hal🦀!").ok();
-    writeln!(serial, "Command helps: ").ok();
-    writeln!(serial, "    led [<none>|on|off|switch]: operate on LED").ok();
+    writeln!(tx, "Welcome to embedded-cli example by bouffalo-hal🦀!").ok();
+    writeln!(tx, "For command helps, type 'help'.").ok();
+
+    let (command_buffer, history_buffer) = ([0; 128], [0; 128]);
+    let mut cli = CliBuilder::default()
+        .writer(tx)
+        .command_buffer(command_buffer)
+        .history_buffer(history_buffer)
+        .prompt("uart-cli-demo> ")
+        .build()
+        .unwrap();
 
     loop {
         led.set_state(led_state).ok();
-        serial.flush().ok();
-
-        write!(serial, "> ").ok();
-
-        let mut idx = 0;
-        while ch == b'\r' || ch == b'\n' {
-            serial.read_exact(core::slice::from_mut(&mut ch)).ok();
-        }
-        while ch != b'\r' && ch != b'\n' && idx < buf.len() {
-            if ch == 0x08 && idx > 0 {
-                // backspace
-                write!(serial, "\x08 \x08").unwrap();
-                idx -= 1;
-            } else if ch != 0x08 {
-                write!(serial, "{}", ch as char).unwrap();
-                buf[idx] = ch;
-                idx += 1;
-            }
-            serial.read_exact(core::slice::from_mut(&mut ch)).ok();
-        }
-        ch = b'\r';
-        writeln!(serial, "").ok();
-        let command = core::str::from_utf8(&buf[..idx]).unwrap();
-
-        match command.trim() {
-            "led" => writeln!(serial, "LED state: {:?}", led_state).unwrap(),
-            "led on" => led_state = PinState::Low,
-            "led off" => led_state = PinState::High,
-            "led switch" => led_state = !led_state,
-            _ => writeln!(serial, "Unknown command: {}", command).unwrap(),
-        }
+        let mut slice = [0];
+        rx.read_exact(&mut slice).ok();
+        let _ = cli.process_byte::<Base, _>(
+            slice[0],
+            &mut Base::processor(|cli, command| {
+                match command {
+                    Base::Hello => {
+                        cli.writer().write_str("Hello world!").ok();
+                    }
+                    Base::Led { command } => match command {
+                        Some(LedCommand::On) => led_state = PinState::Low,
+                        Some(LedCommand::Off) => led_state = PinState::High,
+                        Some(LedCommand::Switch) => led_state = !led_state,
+                        None => match led_state {
+                            PinState::High => cli.writer().write_str("LED state: High").unwrap(),
+                            PinState::Low => cli.writer().write_str("LED state: Low").unwrap(),
+                        },
+                    },
+                }
+                Ok(())
+            }),
+        );
     }
 }
