afd3eca4949b78a3744eb76063f628482afc8b9f
zhenglongbing <1211998648@qq.com>
2025-03-30 22:30:46 +0800

    refactor: format code and adjust indentation
    Signed-off-by: Longbing Zheng <1211998648@qq.com>

--- bouffalo-rt/src/macros.rs
+++ bouffalo-rt/src/macros.rs
@@ -1,20 +1,35 @@
 macro_rules! soc {
-    ($($(#[$doc:meta])* pub struct $Ty: ident => $paddr: expr$(, $DerefTy: ty)+;)+) => {
+    (
         $(
-$(#[$doc])*
-#[allow(non_camel_case_types)]
-pub struct $Ty {
-    _private: (),
-}
-$(
-impl Deref for $Ty {
-    type Target = $DerefTy;
-    #[inline(always)]
-    fn deref(&self) -> &Self::Target {
-        unsafe { &*($paddr as *const _) }
-    }
-}
-)+
+            $(#[$doc:meta])*
+            pub struct $Ty:ident => $paddr:expr_2021
+            $(, $DerefTy:ty)+ ;
+        )+
+    ) => {
+        $(
+            $(#[$doc])*
+            #[allow(non_camel_case_types)]
+            pub struct $Ty {
+                _private: (),
+            }
+
+            $(
+                impl core::ops::Deref for $Ty {
+                    type Target = $DerefTy;
+
+                    #[inline(always)]
+                    fn deref(&self) -> &Self::Target {
+                        unsafe { &*($paddr as *const _) }
+                    }
+                }
+
+                impl core::convert::AsRef<$DerefTy> for $Ty {
+                    #[inline(always)]
+                    fn as_ref(&self) -> &$DerefTy {
+                        unsafe { &*($paddr as *const _) }
+                    }
+                }
+            )+
         )+
     };
 }
